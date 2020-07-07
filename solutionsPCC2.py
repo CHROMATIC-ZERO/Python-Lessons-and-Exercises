04d80c3993bfa089af3f8a18248ffcb1811876a3
CHROMATIC-ZERO <67073317+CHROMATIC-ZERO@users.noreply.github.com>
2020-07-07 14:59:38 -0400

    Add files via upload

---
+++ solutionsPCC2.py
@@ -0,0 +1,46 @@
+# --Chapter 2: VARIABLES AND SIMPLE DATA TYPES--
+# 2.1 - Simple Message:
+msg = "Yo. This is a message from Chromatic_Zero"
+print(msg)
+# 2.2 - Simple Messages:
+msg = "Yo. This is a message from Chromatic_Zero"
+print(msg)
+msg = "And, today! I bring book solutions!"
+print(msg)
+# 2.3 - Personal Message:
+name = "William"
+print(f"Hello {name}. Ready for your daily lessons?")
+# 2.4 - Name Cases:
+name = "Joe"
+print(f"{name.lower()}, {name.upper()}, {name.title()}")
+# 2.5 - Famous Quote:
+print("Philip K. Dick once said:'The basic tool for manipulation of reality is the manipulation of words'")
+# 2.6 - Famous Quote 2:
+famous_person = "Philip K. Dick"
+message = famous_person + " " + "once said:'The basic tool for manipulation of reality is the manipulation of words'"
+print(message)
+# 2.7 - Stripping Names:
+perName = "\tSigurd\n"
+print(perName)
+print(perName.lstrip())
+print(perName.rstrip())
+print(perName.strip())
+# 2.8 - Number Eight:
+print(24/3)
+print(16*0.5)
+print(4+4)
+print(28-20)
+# 2.9 - Favorite Number:
+fNumber = 13
+print("My favourite number is", fNumber)
+# 2.10 - Adding comments:
+# Chromatic_Zero 07.07.2020
+# This program takes a variable containing a number and adds it to a phrase
+fNumber = 13
+print("My favourite number is", fNumber)
+# Chromatic_Zero 06.07.2020
+# This program defines a message to be printed. Then, we define this variable with a new value and print it again
+msg = "Yo. This is a message from Chromatic_Zero"
+print(msg)
+msg = "And, today! I bring book solutions!"
+print(msg)
